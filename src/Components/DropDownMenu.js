05277ecc84edbea60025dcb6e5895d406eb2e98b
Cho Yee Win Aung <choyeewinag>
2021-02-05 10:56:37 -0500

    When schemas been unchecked and addSelectedSchema button has been clicked, it has been removed from the clicked Array.

--- src/Components/DropDownMenu.js
+++ src/Components/DropDownMenu.js
@@ -8,32 +8,126 @@ import CheckBox from './CheckBox';
 const DropDownMenu = ({schemaData}) => {
   const [ clicked, setClicked ] = useState([]);
 
-  const addCheckmark = (e) => {
+  const addCheckmark = (item) => {
     // pass onClick into
     // when clicked it will add specific schema to send to backend
-
     // send data to state
-    const schemaNames = Object.keys(schemaData);// [users, conversations]
+    // const schemaNames = Object.keys(schemaData);// [users, conversations]
 
-    let clickedSchema = e.target.name;
+    let clickedSchema = item.target.name;
 
-    for (let i = 0; i < schemaNames.length; i+=1) {
-      if (schemaNames[i] === clickedSchema && e.target.checked === true) {
-        // setClicked([...clicked, {[currentName]: schemaData[currentName]}])
-        setClicked([...clicked, {[clickedSchema]: schemaData[clickedSchema]}])
-     }
-     else if (schemaNames[i] === clickedSchema && e.target.checked === false) {
-      console.log('hiiii')
-      setClicked(clicked.filter((item) => {
-        Object.keys(item).join('') !== clickedSchema // user = conversions
-        console.log('this is the item[clickedSchema] =====', item[clickedSchema])
-        console.log('item is ', item)
-        console.log('clickedSchema is ', clickedSchema)
-        console.log('schemaNames[i] is ', schemaNames[i])
+    if (clicked.includes(clickedSchema)) {
+      console.log('includes pass')
+      setClicked(clicked.filter(tool => {
+        console.log('filter pass')
+        console.log('tool is ===', tool)
+        return tool !== clickedSchema
       }));
+    } else {
+      setClicked([...clicked, clickedSchema]);
     }
+  }
+    //To-Do
+    // when the add schema button is clicked, we will need to send the schema name to the backend so they can send the schema to the frontend. 
+    // create a function that matches the checked schema names with the key that is in the schemaData object and sends back the object to the backend! 
+    // YAY CHO!!! yaaassss!! <3
 
-    }
+  const checkHandler = e => {
+    const schemaNames = Object.keys(schemaData);
+    const tools = props.schemaNames.clicked; //Array in parent component
+    const value = e.target.value; //Checkbox value
+
+    props.addCheckmark(value);
+  };
+
+  const sendSchemas = (e) => {
+    console.log('clicked array',clicked);
+  }
+
+  const checkBoxComponents = [];
+
+  for (let key in schemaData) {
+    checkBoxComponents.push(
+      <CheckBox name={key} key={`checkbox${key}`} clicked={addCheckmark} />
+    )
+  }
+
+  return(
+    <div>
+      <div>
+      {checkBoxComponents}
+      </div>
+      <div>
+        <button onClick={sendSchemas}>Add Selected Schemas</button>
+      </div>
+
+
+    </div>
+
+  )
+}
+export default DropDownMenu;
+
+
+// output the schema options onto the dropdown menu
+// fetch the schemas from whichever box is checked
+// dropdown menu will display on the left and still be visible after the user chooses the schema
+// stretch: expand and collapse / scroll bar for lots of schemas
+
+
+//create onclick function, if clicked, add this key, to state
+
+  // schemaData = {
+  //   users: {
+
+  //   },
+  //   conversations: {
+
+  //   }
+  // }
+
+  // iterare over all the schema names
+  // create a checkbox for each iteration
+  // const createdCheckBoxes
+
+  //create component of checkbox, map over schemas from backend, pass in new checkbox each time
+  //in checkbox component, have state there
+
+  // const schemas = ['People', 'Places', 'Pets', 'Platters'];
+  //const createdCheckBoxes = schemas.map((schemaName) => {
+    //   return(
+    //     <form>
+    //        <div className="radioButtons">
+    //          <label>
+    //            <input type="checkbox" value={schemaName} onClick={toggleFunction} />
+    //            {schemaName}
+    //            </label>
+    //        </div>
+    //     </form>
+
+    //   )
+    // })
+
+    // map through the schema array
+    // create a new element for each schema (new checkbox)
+    // assign the value to be the current schema
+
+  //   for (let i = 0; i < schemaNames.length; i+=1) {
+      
+  //    if (schemaNames[i].includes(clickedSchema) && e.target.checked === false) {
+  //     console.log('hiiii')
+  //     setClicked(clicked.filter((item) => {
+  //       item.value !== clickedSchema // user = conversions
+  //       console.log('this is the item[clickedSchema] =====', item[clickedSchema])
+  //       console.log('item is ', item)
+  //       console.log('clickedSchema is ', clickedSchema)
+  //       console.log('schemaNames[i] is ', schemaNames[i])
+  //     }));
+  //   } else if (schemaNames[i] === clickedSchema && e.target.checked === true) {
+  //     // setClicked([...clicked, {[currentName]: schemaData[currentName]}])
+  //     setClicked([...clicked, {[clickedSchema]: schemaData[clickedSchema]}])
+  //  }
+  //   }
 
 
     // check if clickedSchema is truthy and target.checked is false
@@ -65,90 +159,4 @@ const DropDownMenu = ({schemaData}) => {
     // console.log('clicked', clicked);
     // console.log(schemaData.users);
     // if the box is unchecked
-    console.log('checked', e.target.checked);
-
-  }
-
-  const sendSchemas = (e) => {
-    console.log('clicked array',clicked);
-  }
-
-
-
-
-  //create onclick function, if clicked, add this key, to state
-
-  // schemaData = {
-  //   users: {
-
-  //   },
-  //   conversations: {
-
-  //   }
-  // }
-
-  const checkBoxComponents = [];
-
-  for (let key in schemaData) {
-    checkBoxComponents.push(
-      <CheckBox name={key} key={`checkbox${key}`} clicked={addCheckmark} />
-    )
-  }
-  // with a for loop
-  //
-
-
-  return(
-    <div>
-      <div>
-      {checkBoxComponents}
-      </div>
-      <div>
-        <button onClick={sendSchemas}>Add Selected Schemas</button>
-      </div>
-
-
-    </div>
-
-  )
-}
-export default DropDownMenu;
-
-
-// output the schema options onto the dropdown menu
-// fetch the schemas from whichever box is checked
-// dropdown menu will display on the left and still be visible after the user chooses the schema
-// stretch: expand and collapse / scroll bar for lots of schemas
-
-
-
-
-
-
-
-
-  // iterare over all the schema names
-  // create a checkbox for each iteration
-  // const createdCheckBoxes
-
-  //create component of checkbox, map over schemas from backend, pass in new checkbox each time
-  //in checkbox component, have state there
-
-  // const schemas = ['People', 'Places', 'Pets', 'Platters'];
-  //const createdCheckBoxes = schemas.map((schemaName) => {
-    //   return(
-    //     <form>
-    //        <div className="radioButtons">
-    //          <label>
-    //            <input type="checkbox" value={schemaName} onClick={toggleFunction} />
-    //            {schemaName}
-    //            </label>
-    //        </div>
-    //     </form>
-
-    //   )
-    // })
-
-    // map through the schema array
-    // create a new element for each schema (new checkbox)
-    // assign the value to be the current schema
+    // console.log('checked', e.target.checked);
