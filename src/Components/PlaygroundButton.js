3616c18ce0434c080546279551d29acfaf89841d
Steven LaBrie <stevenlabrie@yahoo.com>
2021-02-19 19:08:46 -0500

    corrected playgroundButton

--- src/Components/PlaygroundButton.js
+++ src/Components/PlaygroundButton.js
@@ -2,23 +2,9 @@ import React, { useState, useEffect } from "react";
 import '../public/index.css';
 
 const PlaygroundButton = () => {
-
-//   const button = document.getElementById('<your_button_id>');
-//   button.addEventListener('click', () => {
-//   createBrowserWindow();
-// });
-// function createBrowserWindow() {
-//   const remote = require('electron').remote;
-//   const BrowserWindow = remote.BrowserWindow;
-//   const win = new BrowserWindow({
-//     height: 600,
-//     width: 800
-//   });
-//   win.loadURL('<url>');
-// }
   return (
-    <div>
-      <button className="btn draw-border"><a href="http://localhost:3000/graphql" target="_blank"></a>Playground</button>
+    <div className="playgroundButton">
+      <button className="btn draw-border"><a href="http://localhost:3000/graphql" target="_blank">Playground</a></button>
     </div>
   )
 }
