be71c6083ad1943bd5d1cc287980c5bc9f56905a
judy tan <judytan86@gmail.com>
2021-02-20 16:19:49 -0500

    created download button to download zip file and ribbon

--- src/Components/PlaygroundButton.js
+++ src/Components/PlaygroundButton.js
@@ -4,7 +4,7 @@ import '../public/index.css';
 const PlaygroundButton = () => {
   return (
     <div className="playgroundButton">
-      <button className="btn draw-border"><a href="http://localhost:3000/graphql" target="_blank">Playground</a></button>
+      <button className="draw-border"><a href="http://localhost:3000/graphql" target="_blank">Playground</a></button>
     </div>
   )
 }
